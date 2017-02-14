d4595edf31e3b2c7be5e5c8ece7f4c73a9a4e9c2
root <root@liangshuang.qi>
2017-02-14 10:32:43 +0800

    增加AUTH_USER_MODEL配置

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'juser',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +81,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'juser.User'
