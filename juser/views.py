41f87d659364edf4baa0ecde5396c9d9481c7131
root <root@liangshuang.qi>
2017-02-24 15:44:21 +0800

    update

--- juser/views.py
+++ juser/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from juser.user_api import *
 
 
+@defend_attack
 def forget_password(request):
-	pass
+	if request.method == 'POST':
+		pass
+	else:
+		error = '用户名不存在或邮件地址错误'
+
+	return render_to_response('juser/forget_password.html', locals())
